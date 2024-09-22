d707e6b270be5e8251cfc04f2336d8091024550d
bi an <hiephh6663@gmail.com>
2024-09-22 21:56:34 +0700

    first commit

---
+++ src/test/java/com/defty/movie/MovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.defty.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
