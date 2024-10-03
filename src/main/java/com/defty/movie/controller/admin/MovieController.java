009551ed726c29b695522e635b3669851e9baca8
DuDuongVan <duduongvanptit@gmail.com>
2024-10-03 08:50:45 +0700

    update

---
+++ src/main/java/com/defty/movie/controller/admin/MovieController.java
@@ -0,0 +1,14 @@
+package com.defty.movie.controller.admin;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie")
+public class MovieController {
+    @PostMapping("")
+    public void addMovie(){
+
+    }
+}
