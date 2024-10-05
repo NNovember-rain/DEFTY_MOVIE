29f96ae1cbb15fd65b5d998425488ec9b0b1cde9
DuDuongVan <duduongvanptit@gmail.com>
2024-10-06 00:02:53 +0700

    merge hiephd_dev

---
+++ src/main/java/com/defty/movie/dto/response/ArticleResponse.java
@@ -0,0 +1,13 @@
+package com.defty.movie.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ArticleResponse {
+    String massage;
+}
