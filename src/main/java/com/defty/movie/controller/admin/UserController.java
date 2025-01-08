9bbfbcbb7401b15af4fbae62f3977ab4c57110e6
DuDuongVan <duduongvanptit@gmail.com>
2025-01-08 22:03:38 +0700

    api user

--- src/main/java/com/defty/movie/controller/admin/UserController.java
+++ src/main/java/com/defty/movie/controller/admin/UserController.java
@@ -1,15 +1,13 @@
 package com.defty.movie.controller.admin;
 
+import com.defty.movie.dto.response.ApiResponse;
 import com.defty.movie.service.IActorService;
 import com.defty.movie.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Slf4j
@@ -26,5 +24,17 @@ public class UserController {
                              @RequestParam(name = "nationality", required = false) String nationality,
                              @RequestParam(name = "status", required = false) Integer status) {
         return userService.getAllUsers(pageable, name, gender, date_of_birth, nationality, status);
+    }
+
+    @PatchMapping("/status/{id}")
+    @PreAuthorize("@requiredPermission.checkPermission('CHANGE_USER_STATUS')")
+    public ApiResponse<Integer> changeStatus(@PathVariable Integer id) {
+        return userService.changeStatus(id);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("@requiredPermission.checkPermission('GET_USER')")
+    public Object getUser(@PathVariable Integer id){
+        return userService.getUser(id);
     }
 }
