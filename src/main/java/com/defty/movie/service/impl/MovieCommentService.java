040c91af81a1887af8b22314c1ecde72ad8356ba
DuDuongVan <duduongvanptit@gmail.com>
2025-05-16 23:54:21 +0700

    update

--- src/main/java/com/defty/movie/service/impl/MovieCommentService.java
+++ src/main/java/com/defty/movie/service/impl/MovieCommentService.java
@@ -40,13 +40,14 @@ public class MovieCommentService implements IMovieCommentService {
     IAuthUserService authUserService;
     IEpisodeRepository episodeRepository;
     MovieCommentMapper movieCommentMapper;
+    IUserRepository userRepository;
 
     String PREFIX_MOVIE_COMMENT = "MOVIE_COMMENT | ";
 
     @Override
     public Integer addMovieComment(MovieCommentRequest movieCommentRequest) {
         MovieComment movieComment = new MovieComment();
-        Optional<User> user = authUserService.getCurrentUser();
+        Optional<User> user = userRepository.findByUsername(movieCommentRequest.getUsername());
         if(user.isPresent()) {
             log.info(PREFIX_MOVIE_COMMENT + "Get current user success");
             movieComment.setUser(user.get());
